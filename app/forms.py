31d24685236ae3bbfa625db65da0cbe551417cf0
Jahn16 <jpdeoliveiraferreira@gmail.com>
2022-12-24 12:24:26 -0300

    fix: translated messages

--- app/forms.py
+++ app/forms.py
@@ -22,22 +22,36 @@ class LoginForm(FlaskForm):
     email = EmailField(
         "Email",
         validators=[
-            DataRequired(message="Insira um e-mail."),
+            DataRequired("Insira um e-mail."),
             Email("Insira um e-mail válido."),
         ],
     )
     password = PasswordField(
-        "Senha", validators=[DataRequired(message="Insira uma senha.")]
+        "Senha", validators=[DataRequired("Insira uma senha.")]
     )
     remember_me = BooleanField("Lembrar de mim")
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Usuário", validators=[DataRequired()])
-    email = EmailField("Email", validators=[DataRequired()])
-    password = PasswordField("Senha", validators=[DataRequired()])
+    username = StringField(
+        "Usuário", validators=[DataRequired("Insira um usuário.")]
+    )
+    email = EmailField(
+        "Email",
+        validators=[
+            DataRequired("Insira um e-mail."),
+            Email("Insira um e-mail válido."),
+        ],
+    )
+    password = PasswordField(
+        "Senha", validators=[DataRequired("Insira uma senha.")]
+    )
     password_confirmation = PasswordField(
-        "Confirme a senha", validators=[DataRequired(), EqualTo("password")]
+        "Confirme a senha",
+        validators=[
+            DataRequired("Confirme a senha."),
+            EqualTo("password", message="As senhas não coincidem."),
+        ],
     )
 
     def validate_username(self, username):
@@ -54,16 +68,22 @@ class RegistrationForm(FlaskForm):
 class EditProfileForm(FlaskForm):
     previous_username = HiddenField()
     username = StringField(
-        "Usuário", validators=[DataRequired(message="Insira um usuário")]
+        "Usuário", validators=[DataRequired("Insira um usuário.")]
     )
-    about_me = TextAreaField("Descrição", validators=[Length(min=0, max=140)])
+    about_me = TextAreaField(
+        "Descrição",
+        validators=[
+            DataRequired("Insira uma descrição."),
+            Length(min=1, max=140),
+        ],
+    )
 
     def validate_username(self, username):
         if self.previous_username.data == self.username.data:
             return
 
         if User.query.filter_by(username=username.data).first():
-            raise ValidationError("Nome de usuário indisponível")
+            raise ValidationError("Nome de usuário indisponível.")
 
 
 class FollowForm(FlaskForm):
@@ -74,18 +94,30 @@ class PostForm(FlaskForm):
     content = TextAreaField(
         "Diga algo",
         validators=[
-            DataRequired(message="Insira uma mensagem."),
+            DataRequired("Insira uma mensagem."),
             Length(min=1, max=140),
         ],
     )
 
 
 class ForgotPasswordForm(FlaskForm):
-    email = EmailField("E-mail", validators=[DataRequired()])
+    email = EmailField(
+        "E-mail",
+        validators=[
+            DataRequired("Insira um e-mail."),
+            Email("Insira um e-mail válido."),
+        ],
+    )
 
 
 class ChangePasswordForm(FlaskForm):
-    password = PasswordField("Senha", validators=[DataRequired()])
+    password = PasswordField(
+        "Senha", validators=[DataRequired("Insira uma senha.")]
+    )
     password_confirmation = PasswordField(
-        "Confirme a senha", validators=[DataRequired(), EqualTo("password")]
+        "Confirme a senha",
+        validators=[
+            DataRequired("Confirme a nova senha."),
+            EqualTo("password", message="As senhas não coincidem."),
+        ],
     )
